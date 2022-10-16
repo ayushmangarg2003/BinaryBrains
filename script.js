436b6b6093022a6a92e91569c860a9317211b8a4
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2022-10-16 09:48:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,23 +11,24 @@ closeEl.addEventListener('click',()=>{
 })
 
 
-// DarkMode 
-const body = document.querySelector("body"),
-toggle = document.querySelector(".toggle");
+// // DarkMode 
+// const body = document.querySelector("body"),
+// toggle = document.querySelector(".toggle");
 
-let getMode = localStorage.getItem("mode");
-if (getMode && getMode === "dark") {
-body.classList.add("dark");
-toggle.classList.add("active");
-}
+// let getMode = localStorage.getItem("mode");
+// if (getMode && getMode === "dark") {
+// body.classList.add("dark");
+// toggle.classList.add("active");
 
-toggle.addEventListener("click", () => {
-body.classList.toggle("dark");
+// }
 
-if (!body.classList.contains("dark")) {
-  return localStorage.setItem("mode", "light");
-}
-localStorage.setItem("mode", "dark");
-});
+// toggle.addEventListener("click", () => {
+// body.classList.toggle("dark");
 
-toggle.addEventListener("click", () => toggle.classList.toggle("active"));
+// if (!body.classList.contains("dark")) {
+//   return localStorage.setItem("mode", "light");
+// }
+// localStorage.setItem("mode", "dark");
+// });
+
+// toggle.addEventListener("click", () => toggle.classList.toggle("active"));
